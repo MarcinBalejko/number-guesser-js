c2073d320c3452c2d2b264dcc24b2b95bb2e5c7d
MarcinBalejko <mbbm9@o2.pl>
2019-12-21 19:06:21 +0100

    :white_check_mark: Player gets notified when he loses the game

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ guessBtn.addEventListener('click', function () {
 
     // Check if won
     if (guess === winningNum) {
+        // Game over - won
+
         // Disable input
         guessInput.disabled = true
         // Change border color
@@ -35,7 +37,22 @@ guessBtn.addEventListener('click', function () {
         setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
 
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if (guessesLeft === 0) {
+            // Game over - lost
+
+            // Disable input
+            guessInput.disabled = true
+            // Change border color
+            guessInput.style.borderColor = 'red';
+            // Set message
+            setMessage(`Game Over, you lost. The correct number was ${winningNum}`, 'red');
+        } else {
+            // Game continues - answer wrong
+
+        }
     }
 });
 
