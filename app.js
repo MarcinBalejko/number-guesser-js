df40bfbaaa552b685dd034d02c1889b0bfb08fff
MarcinBalejko <mbbm9@o2.pl>
2019-12-21 19:25:09 +0100

    :recycle::fire: Removed repetition in the code

--- app.js
+++ app.js
@@ -29,12 +29,7 @@ guessBtn.addEventListener('click', function () {
     if (guess === winningNum) {
         // Game over - won
 
-        // Disable input
-        guessInput.disabled = true
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        gameOver(true, `${winningNum} is correct, YOU WIN!`);
 
     } else {
         // Wrong number
@@ -43,12 +38,7 @@ guessBtn.addEventListener('click', function () {
         if (guessesLeft === 0) {
             // Game over - lost
 
-            // Disable input
-            guessInput.disabled = true
-            // Change border color
-            guessInput.style.borderColor = 'red';
-            // Set message
-            setMessage(`Game Over, you lost. The correct number was ${winningNum}`, 'red');
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
         } else {
             // Game continues - answer wrong
 
@@ -63,6 +53,22 @@ guessBtn.addEventListener('click', function () {
         }
     }
 });
+
+// Game Over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set message
+    setMessage(msg);
+
+}
 
 // Set message
 function setMessage(msg, color) {
